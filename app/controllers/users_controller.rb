448eb68d351c2f78dd189efd6f604d8c1491f140
Unknown <dhea23mar@gmail.com>
2017-08-29 00:07:02 +0800

    edit account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
